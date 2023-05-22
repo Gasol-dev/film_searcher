a5ce93e68bf9db72822710f530e6d7b8014ca591
Gasol-dev <Gasol-dev@github.com>
2023-05-22 10:28:47 +0300

    Initial commit

---
+++ FilmSearcher/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FilmSearcher
+//
+//  Created by Alexander Lezya on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
